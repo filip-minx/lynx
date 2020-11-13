782c793c02d838f024be6a2ab40c521d91ed6588
Filip Minx <filip.minx2@gmail.com>
2020-11-13 21:40:32 +0100

    Add Repeat operation.

---
+++ Lynx/Operations/Control/Repeat.cs
@@ -0,0 +1,27 @@
+﻿namespace Lynx.Operations.Control
+{
+    public class Repeat : Operation
+    {
+        public override string Identifier => "@";
+
+        public override int Arity => 1;
+
+        public override string VerboseIdentifier => "Repeat";
+
+        public override object[] Execute(Arguments arguments, LynxRuntime runtime)
+        {
+            var iterations = arguments.Get<int>(0);
+
+            var pos = runtime.Tokens.Position;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                runtime.Tokens.Position = pos;
+
+                runtime.ExecuteSubroutine(";");
+            }
+
+            return null;
+        }
+    }
+}
