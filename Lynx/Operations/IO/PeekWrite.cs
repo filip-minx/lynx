475328b6b63edb909bfaa162748a73e0add86fa9
Filip Minx <filip.minx2@gmail.com>
2020-11-14 16:50:17 +0100

    Add PeekWrite and PeekWriteLine operations.

---
+++ Lynx/Operations/IO/PeekWrite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lynx.Operations.IO
+{
+    public class PeekWrite : Operation
+    {
+        public override string Identifier => "W";
+
+        public override string VerboseIdentifier => "PeekWrite";
+
+        public override int Arity => 0;
+
+        public override object[] Execute(Arguments arguments, LynxRuntime runtime)
+        {
+            Console.Write(runtime.Stack.Peek());
+
+            return null;
+        }
+    }
+}
---
+++ Lynx/Operations/IO/PeekWriteLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lynx.Operations.IO
+{
+    public class PeekWriteLine : Operation
+    {
+        public override string Identifier => "L";
+
+        public override string VerboseIdentifier => "PeekWriteLine";
+
+        public override int Arity => 0;
+
+        public override object[] Execute(Arguments arguments, LynxRuntime runtime)
+        {
+            Console.WriteLine(runtime.Stack.Peek());
+
+            return null;
+        }
+    }
+}
